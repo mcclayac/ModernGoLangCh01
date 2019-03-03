dc4b842086fc4a99f72c1fb63bf3a0f2c0a33134
Tony <anthony.mcclay@me.com>
2019-03-03 07:17:39 -0600

    Updated for running on PC

--- dino.go
+++ dino.go
@@ -4,5 +4,8 @@ import "ModernGoLangCh01/dynowebportal"
 
 func main() {
 	//dynowebportal.RunWebPortal("localhost:8787")
-	dynowebportal.RunWebPortal("10.0.0.129:8787")
+	// PC
+	dynowebportal.RunWebPortal("10.0.0.89:8787")
+	// Mac
+	//dynowebportal.RunWebPortal("10.0.0.129:8787")
 }
